a071842f1393d316fb8d763c0bdb47d601ba3293
TranQuangLam <lam.tran.ssg@gmail.com>
2023-02-13 17:29:32 +0700

    Update

---
+++ quiz-client/src/Components/Login.js
@@ -0,0 +1,81 @@
+import {
+  Button,
+  TextField,
+  Box,
+  Card,
+  CardContent,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import useForm from "../Hooks/useForm";
+import Center from "./Center";
+
+const getFreshModel = () => ({
+  name: "",
+  email: "",
+});
+
+export default function Login() {
+  const { values, setValues, errors, setErrors, handleInputChange } = useForm(getFreshModel);
+
+  const login = (e) => {
+    e.preventDefault();
+    if (validate())
+      console.log(values);
+  };
+
+  const validate = () => {
+    let temp = {}
+    temp.email = (/\S+@\S+\.\S+/).test(values.email)?"" : "Email is not valid."
+    temp.name = values.name !== "" ? "" : "This field is required."
+    setErrors(temp);
+    return Object.values(temp).every(x=> x=="");
+  }
+
+  return (
+    <Center>
+      <Card sx={{ width: 400 }}>
+        <CardContent sx={{ textAlign: "center" }}>
+          <Typography variant="h3" sx={{ my: 3 }}>
+            Quiz App
+          </Typography>
+          <Box
+            sx={{
+              "& .MuiTextField-root": {
+                m: 1,
+                width: "90%",
+              },
+            }}
+          >
+            <form noValidate autoComplete="off" onSubmit={login}>
+              <TextField
+                label="Email"
+                onChange={handleInputChange}
+                name="email"
+                value={values.email}
+                variant="outlined"
+                {...(errors.email && {error: true, helperText:errors.email})}
+              />
+              <TextField
+                label="Name"
+                onChange={handleInputChange}
+                name="name"
+                value={values.name}
+                variant="outlined"
+                {...(errors.name && {error: true, helperText:errors.name})}
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                size="large"
+                sx={{ width: "90%" }}
+              >
+                Start
+              </Button>
+            </form>
+          </Box>
+        </CardContent>
+      </Card>
+    </Center>
+  );
+}
